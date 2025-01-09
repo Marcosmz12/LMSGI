48a21ab4ca6289080bb1e0c0590f046ac489889c
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-01-09 17:23:03 +0100

    Ejercicios terminados en clase

---
+++ JavaScript/ejercicio01.js
@@ -0,0 +1,5 @@
+const precio = 200;
+const iva = 21;
+
+let total = precio * (1 + iva /100);
+console.log("El precio total es : " + total);
---
+++ JavaScript/ejercicio02.js
@@ -0,0 +1,5 @@
+const lado = 40;
+const area = lado * lado; 
+const perimetro = 4 * lado;
+console.log("El area es , " + area);
+console.log("El perimetro es , " + perimetro);
