fa5d00f56e955902dd11c8a355c0ae8b052e2ba7
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-04-07 16:22:05 +0200

    subida de emergencia

--- nodejs/server/http.js
+++ nodejs/server/http.js
@@ -2,11 +2,29 @@ const http = require('node:http');
 
 const PORT = 3014;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('Hello World!');
-});
+const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
-server.listen(PORT, () => { 
+    if (req.url === '/') {
+        res.end('<h1>Hola Mundo ! =)</h1>');
+
+    } else if (req.url === '/alumno') {
+        res.setHeader('Content-Type', 'text/json;');
+
+        let alumno = {
+            nombre: Juan,
+            edad: 21
+        };
+
+        res.end(JSON.stringify(alumno));
+    } else {
+        res.statusCode = 404;
+        res.end('<h1>404 Not Found =(</h1>');
+    }
+};
+
+const server = http.createServer(processRequest);
+
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
